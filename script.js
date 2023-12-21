f310b5519a7ad2ffcc55dc923467500f343e18af
strawhatmaceo <maceosylvanbeltman@gmail.com>
2023-12-21 20:22:44 +0100

    removed comments

--- script.js
+++ script.js
@@ -1,31 +1,5 @@
 $(document).ready(function () {
     const name = 'Maceo Beltman';
-    // let beingScrolled = false;
-    // let next = 1;
-
-    // $('#container').on('scroll', () => {
-    //     $('.containers')[1].scrollIntoView({behavior: 'smooth'});
-    //     beingScrolled = true;
-    // });
-
-    // // setInterval(function(){
-    // //     if(beingScrolled){
-    // //         beingScrolled = false;
-
-    // //         $('.containers')[next++].scrollIntoView({behavior: 'smooth'});
-
-    // //         console.log('ended')
-    // //     }
-
-    // //     if(next > 3){
-    // //         next = 0;
-    // //     }
-    // // }, 1000);
-
-    // // $('#container')[0].onscrollend = () => {
-    // //     
-    // // }
-
 
     for (let i = 0; i < name.length; i++) {
         if(i < 5){
